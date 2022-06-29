5f89cf9e7f2056ec3596e5d62edbfff646944227
hyebin yu <hyebinyu1110@gmail.com>
2022-06-29 15:52:28 +0900

    commit until 9.2.1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SassComponent from './SassComponent';
+
+function App() {
+  return (
+    <div>
+      <SassComponent />
+    </div>
+  );
+}
+
+export default App;
